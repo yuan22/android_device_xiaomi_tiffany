b89b2b685878dc784a96c3b6a652b05a6f037589
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-11-11 18:59:20 +0530

    tiffany: Fix init build
    Change-Id: I23def1cb241a62e792fe8bd7bfbead50ebbd2f60

--- init/Android.mk
+++ init/Android.mk
@@ -24,5 +24,6 @@ LOCAL_C_INCLUDES := \
 LOCAL_MODULE := libinit_tiffany
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_tiffany.cpp
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)

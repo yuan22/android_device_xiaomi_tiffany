18bb36a068aca0d312043553eb73736d9c01d903
Alkiyanya <1376106928@qq.com>
2017-12-28 02:30:55 +0800

    tiffany: Initial Tree Push

---
+++ lineage.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/xiaomi/tiffany/full_tiffany.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_tiffany
+PRODUCT_DEVICE := tiffany
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 5X
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="xiaomi/tiffany/tiffany:7.1.2/N2G47H/7.12.14:user/release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi

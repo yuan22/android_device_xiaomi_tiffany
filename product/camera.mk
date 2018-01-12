e07835f8fbf29627294267db52aed4e073ac7403
Alkiyanya <1376106928@qq.com>
2018-01-13 01:25:41 +0800

    tiffany: camera: Import camera HAL * LA.UM.5.6.r1-06100-89xx.0

--- product/camera.mk
+++ product/camera.mk
@@ -1,3 +1,9 @@
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8953 \
+    libmm-qcamera \
+    Snap
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \

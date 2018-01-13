215a2f92632f781bef8a0a9970c7dd315c5b7d1a
Alkiyanya <1376106928@qq.com>
2018-01-13 14:42:31 +0800

    camera: update

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,27 +1,16 @@
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += apq8084
-MM_V4L2_DRIVER_LIST += mpq8092
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8994
-MM_V4L2_DRIVER_LIST += msm8084
-MM_V4L2_DRIVER_LIST += msm8909
-MM_V4L2_DRIVER_LIST += msm8952
-MM_V4L2_DRIVER_LIST += msm8996
-MM_V4L2_DRIVER_LIST += msm8992
-MM_V4L2_DRIVER_LIST += msm8937
-MM_V4L2_DRIVER_LIST += msm8953
-MM_V4L2_DRIVER_LIST += msmcobalt
-MM_V4L2_DRIVER_LIST += msmfalcon
+ifeq ($(call my-dir),$(call project-path-for,qcom-camera))
 
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
+ifneq ($(strip $(USE_DEVICE_SPECIFIC_CAMERA)),true)
+# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
+ifneq ($(TARGET_BOARD_AUTO),true)
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
+      ifneq ($(filter msm8953,$(TARGET_BOARD_PLATFORM)),)
+        include $(addsuffix /Android.mk, $(addprefix $(call my-dir)/, mm-image-codec QCamera2))
+      endif
     endif
   endif
+endif
+endif
+
 endif
